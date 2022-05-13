96a103c82240d45acd2c05fbac3cb0c4c4aa3bc1
MoXuGan <84179961+MoXuGan@users.noreply.github.com>
2022-05-13 18:35:57 +0200

    1er Test

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+import warnings
+warnings.filterwarnings('ignore')
+import os
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import streamlit as st
 
+df = pd.read_csv('archive\coin_Bitcoin.csv')
+st.write("Hello Moi")
